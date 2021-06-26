8aeb4a8b14bf047dddbf479ac9e0f1a8d6c127c8
Domenicomereu97 <domenicomereupalermo@gmail.com>
2021-06-26 19:29:32 +0200

    commentoif

--- frontend/src/Components/Cliente/GestioneAccount/PannelloModifcaPassword.js
+++ frontend/src/Components/Cliente/GestioneAccount/PannelloModifcaPassword.js
@@ -24,7 +24,7 @@ export default class Registrazione extends Component {
 		if (localStorage.getItem("utente") !== null) {
 			let c = JSON.parse(localStorage.getItem("utente"));
 			this.setState({ role: c.role })
-			if (c.role === "guest") {
+		/* 	if (c.role === "guest") {
 				window.location.href = "/ricerca";
 			} else if (c.role === "admin") {
 				window.location.href = "/pannelloAmministratore";
@@ -32,7 +32,7 @@ export default class Registrazione extends Component {
 				window.location.href = "/pannelloParcheggiatore";
 			} else {
 				window.location.href = "/pannelloAutista";
-			}
+			} */
 		} else {
 			window.location.href = "/";
 		}
