cffb936cec4a3133b0c39cabb420ffc932ec2e85
Emanuele Seminara <seminara.emanuele96@gmail.com>
2021-06-27 21:24:34 +0000

    Aggiunte nuove notifiche via email

--- backend/models/mail.js
+++ backend/models/mail.js
@@ -15,10 +15,7 @@ exports.sendWelcomeMail = (email, name) => {
       from: process.env.MAIL_USER,
       to: email,
       subject: "Welcome to Gogoo",
-      text:
-        "Dear " +
-        name +
-        ",\n\nThe Error404 team welcomes you to Gogoo!!",
+      text: "Dear " + name + ",\n\nThe Error404 team welcomes you to Gogoo!!",
     },
     function (error, info) {
       if (error) {
@@ -28,7 +25,7 @@ exports.sendWelcomeMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendPasswordChangedMail = (email, name) => {
   transporter.sendMail(
@@ -49,7 +46,7 @@ exports.sendPasswordChangedMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendRecoveryPasswordMail = (email, password) => {
   transporter.sendMail(
@@ -71,7 +68,7 @@ exports.sendRecoveryPasswordMail = (email, password) => {
       }
     }
   );
-}
+};
 exports.sendInformationChangedMail = (email, name) => {
   transporter.sendMail(
     {
@@ -91,7 +88,7 @@ exports.sendInformationChangedMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendInformationChangedMail = (email, name) => {
   transporter.sendMail(
@@ -112,8 +109,7 @@ exports.sendInformationChangedMail = (email, name) => {
       }
     }
   );
-}
-
+};
 
 exports.sendPaymentMethodRemovedMail = (email, name) => {
   transporter.sendMail(
@@ -134,7 +130,7 @@ exports.sendPaymentMethodRemovedMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendAddedLicenseMail = (email, name) => {
   transporter.sendMail(
@@ -155,7 +151,7 @@ exports.sendAddedLicenseMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendModifiedLicenseMail = (email, name) => {
   transporter.sendMail(
@@ -176,25 +172,88 @@ exports.sendModifiedLicenseMail = (email, name) => {
       }
     }
   );
-}
+};
 
 exports.sendPaymentMethodAddedMail = (email, name) => {
-transporter.sendMail(
-  {
-    from: process.env.MAIL_USER,
-    to: email,
-    subject: "Gogoo - Payment method added",
-    text:
-      "Dear " +
-      name +
-      ",\n\nWe inform you that a payment method has been added from your account.\n\nGogoo Team",
-  },
-  function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
+  transporter.sendMail(
+    {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Gogoo - Payment method added",
+      text:
+        "Dear " +
+        name +
+        ",\n\nWe inform you that a payment method has been added from your account.\n\nGogoo Team",
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
     }
-  }
-);
-}
+  );
+};
+
+exports.sendNewReservationMail = (email, name) => {
+  transporter.sendMail(
+    {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Gogoo - New Reservation",
+      text:
+        "Dear " +
+        name +
+        ",\n\nWe inform you that the booking of your vehicle has been confirmed!\n\nGogoo Team",
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    }
+  );
+};
+
+exports.sendReservationDeletedMail = (email, name, id) => {
+  transporter.sendMail(
+    {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Gogoo - Reservation deleted",
+      text:
+        "Dear " +
+        name +
+        ",\n\nWe inform you that the reservation with id " + id + " has been canceled\n\nGogoo Team",
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    }
+  );
+};
+
+exports.sendReservationEditedMail = (email, name, id) => {
+  transporter.sendMail(
+    {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Gogoo - Reservation deleted",
+      text:
+        "Dear " +
+        name +
+        ",\n\nWe inform you that the reservation with id " + id + " has been edited\n\nGogoo Team",
+    },
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    }
+  );
+};
