c1daf54a3ac64e0a1559f59bbfc513083d7ba1ee
Domenicomereu97 <domenicomereupalermo@gmail.com>
2021-07-01 20:19:02 +0200

    404

--- frontend/src/pages/Errorpage.js
+++ frontend/src/pages/Errorpage.js
@@ -2,31 +2,28 @@ import React, { Component } from "react";
 
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import Cane from "../images/cane_testo.png";
+import Cane from "../images/serverError.png"
 export default class Errorpage extends Component {
-  componentDidMount() {
-    window.localStorage.clear();
-  }
 
-  render() {
-    return (
-      <>
-        <div
-          className="ez flex-column error"
-          style={{
-            backgroundColor: "#1C1927",
-            display: "flex",
-            alignItems: "center",
-            height: "100vh",
-            height: "100%",
-            overflow: "hidden",
-          }}
-        >
-          <Navbar />
-          <img src={Cane} style={{ width: "650px", paddingTop: "20px" }} />
-        </div>
-        <Footer />
-      </>
-    );
-  }
+    componentDidMount() {
+        window.localStorage.clear();
+    }
+
+    render() {
+        return (
+            <>
+            <div className="ez flex-column error" style={{ backgroundColor: "#1C1927", display: "flex", alignItems: "center", height: "100vh", height: "100%", overflow: "hidden"}}>
+                <Navbar />
+
+
+                <img src={Cane} style={{width: "650px", paddingTop: "20px"}}/>
+      
+
+
+               
+            </div>
+             <Footer />
+             </>
+        );
+    }
 }
